881bf22e549500c4c64f356fe981c70abe20d34e
Pratik_Tulkane <pratiktulkane12345@gmail.com>
2021-12-05 09:44:34 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,71 @@
+console.log("HI");
+// Adding Notes into Local Storage
+showNotes();
+let addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click", function(e) {
+    let addTxt = document.getElementById("addTxt");
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    } else {
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.push(addTxt.value);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    addTxt.value = "";
+    // console.log(notesObj);
+    showNotes();
+});
+
+function showNotes() {
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    } else {
+        notesObj = JSON.parse(notes);
+    }
+    let html = "";
+    notesObj.forEach(function(element, index) {
+        html += ` <div class="noteSection">
+                    <div class="noteCards">
+                    <h4>Note ${index+1}</h4>
+                    <p id="notes"> ${element}</p>
+                    <button class="deleteBtn" id="${index}"onclick="deleteNote(this.id)">Delete Note</button>
+                    </div>
+                </div>`;
+    });
+    let notesElm = document.getElementById('notes');
+    if (notesObj.length != 0) {
+        notesElm.innerHTML = html;
+    } else {
+        notesElm.innerHTML = ` Nothing to show Please Select Add Note Button to Add Note`;
+    }
+}
+
+
+// Function to delete a note
+function deleteNote(index) {
+    // console.log("I am deleting", index);
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    } else {
+        notesObj = JSON.parse(notes);
+    }
+
+    notesObj.splice(index, 1);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    showNotes();
+}
+// To Search Note
+
+let search = document.getElementById("searchTxt");
+search.addEventListener("input", function() {
+    let inputVal = search.value;
+    console.log("HI", inputVal);
+    let noteCards = document.getElementsByClassName("noteCards");
+    Array.from(noteCards).forEach(function(element) {
+        let cardTxt = document.getElementsByTagName("p")[0].innerText;
+        console.log(cardTxt);
+    })
+});
